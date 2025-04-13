ed84ea006eae043441a8c01c7befb7a2429a7cb6
eng-Abdallah-daher <abdallahdaher205@gmail.com>
2025-04-13 18:16:43 +0300

    suggestions

--- src/MemoryCardGame.js
+++ src/MemoryCardGame.js
@@ -12,7 +12,6 @@ const IMAGES = [
 
 class MemoryCardGame extends Component {
     timer = null;
-
     constructor() {
         super();
         this.state = {
@@ -35,16 +34,19 @@ class MemoryCardGame extends Component {
         if (this.state.one && this.state.two && prevState.two !== this.state.two) {
             this.handleCardComparison();
         }
-
-        if (!this.state.win) {
+        if (!this.state.win && this.state.cards.length > 0) {
             this.checkWin();
         }
+    }
+
+    isMatch = (card1, card2) => {
+        return card1.src === card2.src;
     }
 
     handleCardComparison = () => {
         const { one, two, cards } = this.state;
         this.setState({ lock: true }, () => {
-            if (one.src === two.src) {
+            if (this.isMatch(one, two)) {
                 const updatedCards = cards.map(card =>
                     card.src === one.src ? { ...card, matched: true } : card
                 );
@@ -68,9 +70,7 @@ class MemoryCardGame extends Component {
         clearInterval(this.timer);
         let list = [...IMAGES, ...IMAGES];
         list = this.shuffle(list);
-
         const mix = list.map((c, index) => ({ ...c, id: `${c.src}-${index}` }));
-
         this.setState({
             cards: mix,
             one: null,
@@ -90,21 +90,17 @@ class MemoryCardGame extends Component {
     shuffle = (array) => {
         let currentIndex = array.length;
         let randomIndex;
-
         while (currentIndex !== 0) {
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
-
             [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
         }
-
         return array;
     }
 
     pick = (card) => {
         const { one, two, lock } = this.state;
         if (lock || card === one || card.matched) return;
-
         if (!one) {
             this.setState({ one: card });
         } else if (!two) {
@@ -126,11 +122,10 @@ class MemoryCardGame extends Component {
         return (
             <div className="page">
                 <div className="box">
-                    <h1>memory game</h1>
-                    <button onClick={this.start}>new game</button>
-                    <div className="info">moves: {moves} | time: {time}s</div>
-                    {win && <div className="info" style={{ color: 'green', fontWeight: 'bold' }}>you win!</div>}
-
+                    <h1 tabIndex={0}>Memory Game</h1>
+                    <button onClick={this.start}>New Game</button>
+                    <div className="info" tabIndex={0}>Moves: {moves} | Time: {time}s</div>
+                    {win && <div className="info" style={{ color: 'green', fontWeight: 'bold' }} tabIndex={0}>You win!</div>}
                     <div className="grid">
                         {cards.map(card => (
                             <Card
@@ -148,9 +143,27 @@ class MemoryCardGame extends Component {
 }
 
 const Card = ({ card, isFlipped, onClick }) => (
-    <div className={`card${isFlipped ? ' flipped' : ''}`} onClick={onClick}>
+    <div
+        className={`card${isFlipped ? ' flipped' : ''}${card.matched ? ' matched' : ''}`}
+        onClick={onClick}
+        onKeyDown={(e) => {
+            if ((e.key === 'Enter' || e.key === ' ') && !card.matched) {
+                e.preventDefault();
+                onClick();
+            }
+        }}
+        tabIndex={card.matched ? 0 : 0}
+        role="button"
+        aria-label={
+            card.matched
+                ? `Card matched: ${card.src.split('/').pop()}`
+                : isFlipped
+                    ? `Card open: ${card.src.split('/').pop()}`
+                    : 'Hidden card'
+        }
+    >
         <div className="front">
-            <img src={card.src} alt="card" />
+            <img src={card.src} alt="" />
         </div>
         <div className="back">?</div>
     </div>
