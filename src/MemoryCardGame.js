248a3f50a2cd28bbd833f765c641f4d586578b41
eng-Abdallah-daher <abdallahdaher205@gmail.com>
2025-04-13 17:18:18 +0300

    improvement

--- src/MemoryCardGame.js
+++ src/MemoryCardGame.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import './MemoryCardGame.css';
+
+const IMAGES = [
+    { src: 'https://www.helpfulgames.com/nativeGames/memory/bilder/image015.png', matched: false },
+    { src: 'https://www.helpfulgames.com/nativeGames/memory/bilder/image040.png', matched: false },
+    { src: 'https://www.helpfulgames.com/nativeGames/memory/bilder/image027.png', matched: false },
+    { src: 'https://www.helpfulgames.com/nativeGames/memory/bilder/image030.png', matched: false },
+    { src: 'https://www.helpfulgames.com/nativeGames/memory/bilder/image006.png', matched: false },
+    { src: 'https://www.helpfulgames.com/nativeGames/memory/bilder/image008.png', matched: false }
+];
+
 class MemoryCardGame extends Component {
     timer = null;
+
     constructor() {
         super();
         this.state = {
@@ -14,48 +25,51 @@ class MemoryCardGame extends Component {
             started: false,
             win: false
         };
-
-        this.imgs = [
-            { src: 'https://www.helpfulgames.com/nativeGames/memory/bilder/image015.png', matched: false },
-            { src: 'https://www.helpfulgames.com/nativeGames/memory/bilder/image040.png', matched: false },
-            { src: 'https://www.helpfulgames.com/nativeGames/memory/bilder/image027.png', matched: false },
-            { src: 'https://www.helpfulgames.com/nativeGames/memory/bilder/image030.png', matched: false },
-            { src: 'https://www.helpfulgames.com/nativeGames/memory/bilder/image006.png', matched: false },
-            { src: 'https://www.helpfulgames.com/nativeGames/memory/bilder/image008.png', matched: false }
-        ];
     }
 
     componentWillUnmount() {
         clearInterval(this.timer);
     }
 
-    componentDidUpdate(prev1, prev) {
-        const { one, two, cards, win } = this.state;
-        if (one && two && prev.two !== two) {
-            this.setState({ lock: true }, () => {
-                if (one.src === two.src) {
-                    this.setState({
-                        cards: cards.map(c => c.src === one.src ? { ...c, matched: true } : c)
-                    }, this.reset);
-                } else {
-                    setTimeout(this.reset, 1000);
-                }
-            });
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.one && this.state.two && prevState.two !== this.state.two) {
+            this.handleCardComparison();
         }
-        if (!win) {
-            let done = this.state.cards.length > 0 && this.state.cards.every(c => c.matched);
-            if (done) {
-                this.setState({ win: true });
-                clearInterval(this.timer);
+
+        if (!this.state.win) {
+            this.checkWin();
+        }
+    }
+
+    handleCardComparison = () => {
+        const { one, two, cards } = this.state;
+        this.setState({ lock: true }, () => {
+            if (one.src === two.src) {
+                const updatedCards = cards.map(card =>
+                    card.src === one.src ? { ...card, matched: true } : card
+                );
+                this.setState({ cards: updatedCards }, this.resetSelection);
+            } else {
+                setTimeout(this.resetSelection, 1000);
             }
+        });
+    }
+
+    checkWin = () => {
+        const { cards } = this.state;
+        const allMatched = cards.length > 0 && cards.every(card => card.matched);
+        if (allMatched) {
+            this.setState({ win: true });
+            clearInterval(this.timer);
         }
     }
 
     start = () => {
         clearInterval(this.timer);
-        let list = [...this.imgs, ...this.imgs];
-        list.sort(() => Math.random() - 0.5);
-        let mix = list.map(c => ({ ...c, id: Math.random() }));
+        let list = [...IMAGES, ...IMAGES];
+        list = this.shuffle(list);
+
+        const mix = list.map((c, index) => ({ ...c, id: `${c.src}-${index}` }));
 
         this.setState({
             cards: mix,
@@ -68,26 +82,41 @@ class MemoryCardGame extends Component {
             win: false
         }, () => {
             this.timer = setInterval(() => {
-                this.setState(s => ({ time: s.time + 1 }));
+                this.setState(state => ({ time: state.time + 1 }));
             }, 1000);
         });
     }
 
-    pick = c => {
+    shuffle = (array) => {
+        let currentIndex = array.length;
+        let randomIndex;
+
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+        }
+
+        return array;
+    }
+
+    pick = (card) => {
         const { one, two, lock } = this.state;
-        if (lock || c === one || c.matched) return;
+        if (lock || card === one || card.matched) return;
+
         if (!one) {
-            this.setState({ one: c });
+            this.setState({ one: card });
         } else if (!two) {
-            this.setState({ two: c });
+            this.setState({ two: card, lock: true });
         }
     }
 
-    reset = () => {
-        this.setState(s => ({
+    resetSelection = () => {
+        this.setState(state => ({
             one: null,
             two: null,
-            moves: s.moves + 1,
+            moves: state.moves + 1,
             lock: false
         }));
     }
@@ -96,8 +125,6 @@ class MemoryCardGame extends Component {
         const { cards, one, two, moves, time, win } = this.state;
         return (
             <div className="page">
-            
-
                 <div className="box">
                     <h1>memory game</h1>
                     <button onClick={this.start}>new game</button>
@@ -105,22 +132,28 @@ class MemoryCardGame extends Component {
                     {win && <div className="info" style={{ color: 'green', fontWeight: 'bold' }}>you win!</div>}
 
                     <div className="grid">
-                        {cards.map(c => {
-                            let flip = c === one || c === two || c.matched;
-                            return (
-                                <div key={c.id} className={`card${flip ? ' flipped' : ''}`} onClick={() => this.pick(c)}>
-                                    <div className="front">
-                                        <img src={c.src} alt="card" />
-                                    </div>
-                                    <div className="back">?</div>
-                                </div>
-                            );
-                        })}
+                        {cards.map(card => (
+                            <Card
+                                key={card.id}
+                                card={card}
+                                isFlipped={card === one || card === two || card.matched}
+                                onClick={() => this.pick(card)}
+                            />
+                        ))}
                     </div>
                 </div>
             </div>
         );
     }
 }
+
+const Card = ({ card, isFlipped, onClick }) => (
+    <div className={`card${isFlipped ? ' flipped' : ''}`} onClick={onClick}>
+        <div className="front">
+            <img src={card.src} alt="card" />
+        </div>
+        <div className="back">?</div>
+    </div>
+);
 
 export default MemoryCardGame;
